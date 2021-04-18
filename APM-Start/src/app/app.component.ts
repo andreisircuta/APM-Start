5fa2ab6811510265f07169f19844265f8d5714ec
pengu <pengu89@gmail.com>
2021-04-18 15:09:16 +0300

    Creating some basic product-list

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -2,14 +2,7 @@ import {Component} from "@angular/core";
 
 @Component({
   selector: 'pm-root',
-  template: `
-    <div>
-      <h1>
-        {{pageTitle}}
-      </h1>
-      <div>My first componenta</div>
-    </div>
-  `
+  templateUrl: './app.component.html'
 })
 export class AppComponent{
   pageTitle: string = "Acme Product Management";
