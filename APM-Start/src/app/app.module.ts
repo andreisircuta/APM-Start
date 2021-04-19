0cce42485af4c337d208e5f3be1911ed160f6061
andrei.sircuta <andrei.sircuta@gmail.com>
2021-04-19 17:31:55 +0300

    Splitting the application module and creating product.module.ts

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -3,39 +3,24 @@ import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 
 import {AppComponent} from './app.component';
-import {ProductListComponent} from './products/product-list.component';
-import {FormsModule} from '@angular/forms';
-import {ConvertToSpacesPipe} from './shared/convert-to-spaces.pipe';
-import {StarComponent} from './shared/star/star.component';
-import {ProductDetailsComponent} from './products/product-details.component';
 import {WelcomeComponent} from './home/welcome.component';
 import {RouterModule} from '@angular/router';
-import {ProductDetailGuard} from './products/product-detail.guard';
+import {ProductModule} from './products/product.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
-      {path: 'products',  component: ProductListComponent},
-      {
-        path: 'products/:id',
-        component:ProductDetailsComponent,
-        canActivate:[ProductDetailGuard]
-      },
       {path: 'welcome', component:WelcomeComponent},
       {path: '', redirectTo:'welcome', pathMatch: 'full'},
       {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
-    ])
+    ]),
+    ProductModule
   ],
   bootstrap: [AppComponent]
 })
