7e1963e0374e604348530fd4c001517b6d1ca6fa
andrei.sircuta <andrei.sircuta@gmail.com>
2021-04-19 12:33:19 +0300

    Adding some basic routing

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
-import {ProductListComponent} from "./products/product-list.component";
-import {FormsModule} from "@angular/forms";
-import {ConvertToSpacesPipe} from "./shared/convert-to-spaces.pipe";
-import {StarComponent} from "./shared/star/star.component";
-import { ProductDetailsComponent } from './products/product-details.component';
+import {AppComponent} from './app.component';
+import {ProductListComponent} from './products/product-list.component';
+import {FormsModule} from '@angular/forms';
+import {ConvertToSpacesPipe} from './shared/convert-to-spaces.pipe';
+import {StarComponent} from './shared/star/star.component';
+import {ProductDetailsComponent} from './products/product-details.component';
 import {WelcomeComponent} from './home/welcome.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,14 @@ import {WelcomeComponent} from './home/welcome.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'products',  component: ProductListComponent},
+      {path: 'products/:id', component:ProductDetailsComponent},
+      {path: 'welcome', component:WelcomeComponent},
+      {path: '', redirectTo:'welcome', pathMatch: 'full'},
+      {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+    ])
   ],
   bootstrap: [AppComponent]
 })
